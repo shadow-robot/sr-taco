1e764b046c65c180e38ada2d39dfbc2c36f5ffbe
Mark Pitchless <markpitchless@gmail.com>
2012-09-10 16:36:58 +0100

    Re-publish the point clounds from openni as TACO point clouds.

--- sr_taco_openni/include/sr_taco_openni/sr_taco_openni.h
+++ sr_taco_openni/include/sr_taco_openni/sr_taco_openni.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "ros/ros.h"
+#include "sensor_msgs/PointCloud2.h"
 
 namespace sr_taco_openni {
+
+using namespace std;
+using namespace ros;
 
 class TacoOpenNI {
     public:
@@ -10,7 +14,16 @@ class TacoOpenNI {
         ~TacoOpenNI() {}
 
     private:
-        ros::NodeHandle nh, nh_home;
+        NodeHandle nh, nh_home;
+
+        // Namespace to find the camera in.
+        string camera;
+
+        Subscriber pclSub;
+        void pclIn(const sensor_msgs::PointCloud2::ConstPtr& msg);
+
+        Publisher unfoveatedPointCloudPub;
+        Publisher foveatedPointCloudPub;
 };
 
 } // sr_taco_openni::
--- sr_taco_openni/src/TacoOpenNI.cpp
+++ sr_taco_openni/src/TacoOpenNI.cpp
@@ -2,6 +2,21 @@
 
 namespace sr_taco_openni {
 
-    TacoOpenNI::TacoOpenNI() : nh_home("~") {}
+    TacoOpenNI::TacoOpenNI() : nh_home("~") {
+        nh_home.param<string>("camera", camera, "camera");
+
+        // Just use depth not depth_registed as we don't need RGB/Depth alignment
+        pclSub = nh.subscribe(camera + "/depth/points", 1, &TacoOpenNI::pclIn, this);
+
+        foveatedPointCloudPub = nh_home.advertise<sensor_msgs::PointCloud2>(
+                "foveated/pointcloud2", 5);
+        unfoveatedPointCloudPub = nh_home.advertise<sensor_msgs::PointCloud2>(
+                "unfoveated/pointcloud2", 5);
+    }
+        
+    void TacoOpenNI::pclIn(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+        foveatedPointCloudPub.publish(msg);
+        unfoveatedPointCloudPub.publish(msg);
+    }
 
 } // sr_taco_openni::
