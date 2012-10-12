99920e113fce5f2365ece60c0e46bc8a29e35da7
markpitchless <markpitchless@gmail.com>
2012-10-12 16:19:32 +0100

    Add VoxGrid filter example.

--- sr_pcl_tracking/src/example.cpp
+++ sr_pcl_tracking/src/example.cpp
@@ -6,24 +6,32 @@
 #include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 ros::Publisher pub;
 
 void
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud)
 {
-  // ... do data processing
+  sensor_msgs::PointCloud2 cloud_filtered;
 
-  sensor_msgs::PointCloud2 output;
+  float leaf_size = 0.01;
+
+  // Perform the actual filtering
+  pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+  sor.setInputCloud (cloud);
+  sor.setLeafSize (leaf_size, leaf_size, leaf_size);
+  sor.filter (cloud_filtered);
+
   // Publish the data
-  pub.publish (output);
+  pub.publish (cloud_filtered);
 }
 
 int
 main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "sr_pcl_tracking");
+  ros::init (argc, argv, "pcl_voxelgrid");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
