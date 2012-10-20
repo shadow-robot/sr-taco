ee6f8a1c4e70e9ffd3758bcf00283da1e3e0ec39
markpitchless <markpitchless@gmail.com>
2012-10-20 22:29:43 +0100

    Move to using a proxy object for tracker comms.

--- sr_pcl_tracking_gui/src/sr_pcl_tracking_gui/sr_pcl_tracking_gui.py
+++ sr_pcl_tracking_gui/src/sr_pcl_tracking_gui/sr_pcl_tracking_gui.py
@@ -9,12 +9,39 @@ from python_qt_binding.QtGui import QWidget
 
 from std_srvs.srv import *
 
+class SrPclTrackingProxy(object):
+    """Provides a proxy interface to the sr_pcl_tracking/sr_pcl_tracker node.
+    Call methods on an instance calls services etc on the tracker.
+    Pass the node to proxy to py passing the name to the constructor. Default
+    is /sr_pcl_tracker
+    """
+    def __init__(self, topic = "/sr_pcl_tracker"):
+        self.topic = topic
+
+    def track_centered(self):
+        self.callEmptySrv(self.topic + "/track_centered")
+
+    def track_nearest(self):
+        self.callEmptySrv(self.topic + "/track_nearest")
+
+    def callEmptySrv(self, name):
+        rospy.wait_for_service(name);
+        try:
+            srv_proxy = rospy.ServiceProxy(name, Empty)
+            resp = srv_proxy()
+            return resp
+        except rospy.ServiceException, e:
+            rospy.logerr("%s service fail: %s"%(name, e))
+
+
 class SrPclTrackingGui(Plugin):
 
     def __init__(self, context):
         super(SrPclTrackingGui, self).__init__(context)
         # give QObjects reasonable names
         self.setObjectName('SrPclTrackingGui')
+
+        self.tracker = SrPclTrackingProxy("/sr_pcl_tracker");
 
         # create QWidget
         self.ui = QWidget()
@@ -28,11 +55,9 @@ class SrPclTrackingGui(Plugin):
         # add widget to the user interface
         context.add_widget(self.ui)
 
-        self.ui.trackCenteredBtn.pressed.connect(
-                self.mk_emptySrvCall('/sr_pcl_tracker/track_centered') )
-        self.ui.trackNearestBtn.pressed.connect(
-                self.mk_emptySrvCall('/sr_pcl_tracker/track_nearest') )
-        
+        self.ui.trackCenteredBtn.pressed.connect(self.tracker.track_centered)
+        self.ui.trackNearestBtn.pressed.connect(self.tracker.track_nearest)
+
         #self.ui.refreshBtn
         #self.ui.saveBtn
         #self.ui.loadBtn
@@ -54,16 +79,3 @@ class SrPclTrackingGui(Plugin):
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure it
         # Usually used to open a dialog to offer the user a set of configuration
-
-    def mk_emptySrvCall(self, name):
-        """Creates a callback that calls the passed service name, which must be using std_srvs.Empty.
-        """
-        def emptySrvCall():
-            rospy.wait_for_service(name);
-            try:
-                srv_proxy = rospy.ServiceProxy(name, Empty)
-                resp = srv_proxy()
-                return
-            except rospy.ServiceException, e:
-                rospy.logerr("%s service fail: %s"%(name, e))
-        return emptySrvCall
