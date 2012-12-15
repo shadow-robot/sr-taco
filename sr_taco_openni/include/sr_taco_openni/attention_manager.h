ae9202bbf29142912b761680f8c9940ef60b36de
markpitchless <markpitchless@gmail.com>
2012-12-15 00:58:14 +0000

    Skeleton nodelot plugin for AttentionManager.
    The first manager, ClusterSegment is loadable with:
    
     $ rosrun nodelet nodelet manager __name:=nodelet_manager
     $ rosrun nodelet nodelet load sr_taco_openni/cluster_segment nodelet_manager __name:=nodelet1
     [ INFO] [1355532851.190470831]: Loading nodelet /nodelet1 of type sr_taco_openni/cluster_segment to manager nodelet_manager with the following remappings:
     [ INFO] [1355532851.194129791]: Hello from nodelet number 1 :D

---
+++ sr_taco_openni/include/sr_taco_openni/attention_manager.h
@@ -0,0 +1,22 @@
+/*
+ * attention_manager.h
+ *
+ *  Created on: 14 Dec 2012
+ *      Author: mda
+ */
+
+#pragma once
+
+#include <nodelet/nodelet.h>
+
+namespace sr_taco_openni
+{
+  /**
+   * \brief Base class for attention managers, nodelets that generate saliency maps.
+   */
+  class AttentionManager : public nodelet::Nodelet
+  {
+    public:
+      AttentionManager(){}
+  };
+}
---
+++ sr_taco_openni/src/cluster_segment.cpp
@@ -0,0 +1,31 @@
+/*
+ * cluster_segment.cpp
+ *
+ *  Created on: 14 Dec 2012
+ *      Author: mda
+ */
+
+#include <pluginlib/class_list_macros.h>
+#include <sr_taco_openni/attention_manager.h>
+#include <ros/ros.h>
+
+namespace sr_taco_openni
+{
+  /**
+   * @brief AttentionManager that extracts clusters after removing planar surfaces.
+   */
+  class ClusterSegment : public AttentionManager
+  {
+    public:
+      ClusterSegment(){}
+
+    private:
+      virtual void onInit()
+      {
+        NODELET_INFO("Hello from nodelet number 1 :D");
+      }
+
+  };
+}
+
+PLUGINLIB_DECLARE_CLASS(sr_taco_openni,cluster_segment,sr_taco_openni::ClusterSegment,nodelet::Nodelet)
