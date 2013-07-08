86cb1e5241bf9f2fb201b4a1853c0b9d4162dade
Mark Pitchless <markpitchless@gmail.com>
2013-07-08 13:07:14 +0100

    Fill in missing data from last row

--- sr_taco_benchmarking/bin/bag_cloud_count.py
+++ sr_taco_benchmarking/bin/bag_cloud_count.py
@@ -22,6 +22,13 @@ for topic, msg, t in bag.read_messages(topics=[result_topic, total_topic]):
 bag.close()
 
 writer = csv.writer(sys.stdout, delimiter=',')
-writer.writerow(['bag_time', 'total_points', 'result_points'])
+writer.writerow(['bag_time', 'total_points', 'result_points_raw'])
+# We track the last row to fill in gaps in the current row using the last value
+# seen. ie the last value published
+last_row = { 't': 0.0, 'total': None, 'result': None  }
 for row in results:
-    writer.writerow([row['t'], row['total'], row['result']])
+    this_row = {}
+    for k in row.keys():
+        this_row[k] = row[k] if row[k] else last_row[k]
+    writer.writerow([this_row['t'], this_row['total'], this_row['result']])
+    last_row = this_row
